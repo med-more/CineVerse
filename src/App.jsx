33aa3ce8f1d69ef858dfa9d29eec76471cc14e22
SAMIR EL ALAMI <elalamisamirr@gmail.com>
2025-11-20 15:25:34 +0100

    new navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import Navbar from './components/navbar.jsx';
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import Contact from "./pages/Contact";
 
-
-function App() {
-
+export default function App() {
   return (
     <>
-      <Navbar/>
-    </>
-  )
-}
+      <Navbar />
 
-export default App
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <nav className='ourNav'>
-      <h1>Cineverse</h1>
-      <ul>
-        <li>Home</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-  )
-}
+    <nav className="navbar">
+      <h2 className="logo">CineVerse</h2>
 
-export default Navbar
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </nav>
+  );
+}
