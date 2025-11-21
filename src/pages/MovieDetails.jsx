788ed7f6a8edae6f213603008a0a4139df2c0415
Mohammed Baba <mohammedbaba1505@gmail.com>
2025-11-21 15:28:08 +0100

    add MovieDetails logic with params, fallback tags, rating and duration handling

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useParams, Link } from "react-router-dom"
+import { movies } from "../data/movies"
+import poster from "../assets/images/fastx.jpeg"
 
+const FALLBACK_TAGS = ["action", "Adventure"];
 const MovieDetails = () => {
+    const { id } = useParams();
+    const movie = movies.find((m) =>m.id === Number(id));
+    if(!movie) return <h2>Film non trouve</h2>
+
+    const tags = movie.tags && movie.tags.length ? movie.tags : FALLBACK_TAGS;
+    const rating = movie.rating ?? "8.0";
+    const duration = movie.duration ?? "2h 05";
   return (
-    <div>MovieDetails</div>
+    <></>
   )
 }
 
